93faf4ccabd7838b7936b6e0009c0cc4196497e2
elmaayouf <boubkerelmaayouf@gmail.com>
2023-04-25 12:09:10 -0700

    the file that containes the specifiers functions

--- main_printf/main.h
+++ main_printf/main.h
@@ -10,7 +10,7 @@
 #define BUF_OUTPUT_SIZE 1024 /*task 4*/
 #define BUFFER_FLUSH -1
 
-#define FIELD_ BUF_SIZE 50
+#define FIELD_BUF_SIZE 50
 
 #define NULL_STRING "(null)"
 
@@ -51,7 +51,6 @@ typedef struct _parameters
 
 /**
  * struct specifier - Struct token
- *
  * @specifier: format token
  * @f: The function associated
  */
@@ -77,7 +76,7 @@ int print_S(va_list ap, prm_t *params);
 
 /*** number functions  | located in print_numb.c file ***/
 char *convert(long int num, int base, int flags, prm_t *params);
-int print_unsigned(va_list ap, prm_t *params); /*task 3*/
+int print_unsigned(va_list ap, prm_t *params);
 int print_address(va_list ap, prm_t *params);
 
 /*** convert_number.c | located in the convert_numbers.c file ***/
@@ -88,7 +87,7 @@ int print_octal(va_list ap, prm_t *params);
 
 /*** simple printers functions | located in  ***/
 int print_from_to(char *start, char *stop, char *except);
-int print_rev(va_list ap, prm_t *params); /* task 13 */
+int print_rev(va_list ap, prm_t *params);
 int print_rot13(va_list ap, prm_t *params);
 
 /*** print numbers functions | located in the print_numbers.c file ***/
---
+++ main_printf/specifier.c
@@ -0,0 +1,141 @@
+#include "main.h"
+
+/**
+ * get_specifier - this function finds the format function
+ * @s: A string containing the format specifier to be matched.
+ * Return: corresponding print function else, returns NULL.
+ */
+int (*get_specifier(char *s))(va_list ap, prm_t *params)
+
+{
+	speci_t specifiers[] = {
+		{"c", print_char},
+		{"d", print_int},
+		{"i", print_int},
+		{"s", print_string},
+		{"%", print_percent},
+		{"b", print_binary},
+		{"o", print_octal},
+		{"u", print_unsigned},
+		{"x", print_hexadecimal},
+		{"X", print_HEXADECIMAL},
+		{"p", print_address},
+		{"S", print_S},
+		{"r", print_rev},
+		{"R", print_rot13},
+		{NULL, NULL}
+	};
+	int i;
+
+	for (i = 0; specifiers[i].specifiers != NULL; i++)
+	{
+		if (*s == specifiers[i].specifiers[0])
+		{
+			return (specifiers[i].f);
+		}
+	}
+
+	return (NULL);
+}
+
+/**
+ * get_print_func - finds the format function
+ * @s: string of the format
+ * @ap: argument pointer
+ * @params: the parameters struct
+ * Return: the number of bytes printed
+ */
+
+int get_print_func(char *s, va_list ap, prm_t *params)
+{
+	int (*f)(va_list, prm_t *) = get_specifier(s);
+
+	if (f)
+		return (f(ap, params));
+	return (0);
+}
+
+/**
+ * get_flag - This function identifies the flag in the format string.
+ * @s: The format string
+ * @params: The parameters struct
+ * Return: The integer value of the identified flag.
+ */
+
+int get_flag(char *s, prm_t *params)
+
+{
+	int flag_value = 0;
+
+	switch (*s)
+	{
+		case '+':
+			flag_value = (*params).plus_f = 1;
+			break;
+		case ' ':
+			flag_value = (*params).space_f = 1;
+			break;
+		case '#':
+			flag_value = (*params).hashtag_f = 1;
+			break;
+		case '-':
+			flag_value = (*params).minus_f = 1;
+			break;
+		case '0':
+			flag_value = (*params).zero_f = 1;
+			break;
+	}
+	return (flag_value);
+}
+
+/**
+ * get_modifier - this function Identifies and sets the value of a modifier
+ *  in the format string
+ * @s: The format string
+ * @params: The parameters struct
+ * Return: The integer value of the identified modifier.
+ */
+int get_modifier(char *s, prm_t *params)
+{
+	int modifier_set = 0;
+
+	switch (*s)
+	{
+		case 'h':
+			modifier_set = (*params).modifier_h = 1;
+			break;
+		case 'l':
+			modifier_set = (*params).modifier_l = 1;
+			break;
+	}
+	return (modifier_set);
+}
+
+/**
+ * *get_width - this function Extracts and sets
+ * the width from the format string.
+ * @s: the format string
+ * @params: A struct containing formatting parameters for the string.
+ * @ap: A pointer to a variable argument list containing arguments to format.
+ * Return: A new pointer
+ */
+char *get_width(char *s, prm_t *params, va_list ap)
+{
+	int width_value = 0;
+
+	if (*s == '*')
+	{
+		width_value = va_arg(ap, int);
+		s++;
+	}
+	else
+	{
+		for (; _isdigit(*s); s++)
+		{
+			width_value = width_value * 10 + (*s - '0');
+		}
+	}
+	(*params).widths = width_value;
+	return (s);
+}
+
